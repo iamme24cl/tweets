a64ed72099152f06724e5b91b89df87a8d148faa
Chandra Lama <iamme24cl@gmail.com>
2024-02-06 14:19:17 -0800

    add routes

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import Home from "./Home";
 import Navbar from "./Navbar";
+import Login from "./Login";
+import Register from "./Register";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
     return (
         <React.Fragment>
             <Navbar />
-            <Home />
+            <Router>
+                <Routes>
+                    <Route path="/" exact element={<Home />} />
+                    <Route path="/login" exact element={<Login />} />
+                    <Route path="/register" exact element={<Register />} />
+                </Routes>
+            </Router>
         </React.Fragment>
     );
 }
